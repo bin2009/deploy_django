90adb97534bdb713028e6a6cc3f025adb7943b80
bin2009 <npbloc.cntt@gmail.com>
2024-11-20 23:53:01 +0700

    update20

--- members/urls.py
+++ members/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
-    path('students/', views.students, name='students'),
+    path('tutors/', views.students, name='students'),
 ]
