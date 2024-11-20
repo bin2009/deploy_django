ca6fc9fdbff3a531e557c73c561c5e4a7d07e6f5
bin2009 <npbloc.cntt@gmail.com>
2024-11-21 00:14:02 +0700

    test sql

--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name='members'),
     path('tutors/', views.students, name='students'),
+    path('create_user/', views.create_user, name='create_user'),
+    path('get_all_users/', views.get_all_users, name='get_all_users'),  # New URL pattern
 ]
--- members/views.py
+++ members/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password
+
 
 def members(request):
     return HttpResponse("Hello world!")
 
 def students(request):
     return HttpResponse("page for students!")
+
+def create_user(request):
+    if request.method == 'GET':
+        username = 'BAOLOC2'
+        password = '123123'
+        user = User.objects.create(username=username, password=password)
+        return JsonResponse({'status': 'create success'}, status=200)
+    return JsonResponse({'status': 'Invalid request method'}, status=405)
+
+def get_all_users(request):
+    if request.method == 'GET':
+        users = User.objects.all().values('id', 'username', 'email', 'first_name', 'last_name')
+        users_list = list(users)
+        return JsonResponse(users_list, safe=False, status=200)
+    return JsonResponse({'status': 'Invalid request method'}, status=405)
